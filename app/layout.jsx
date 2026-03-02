0ae5811d125a8690519086275adf83e93db01012
Sagar Kumar <sagarkumar15839@gmail.com>
2026-03-02 10:45:27 +0530

    implemented message count

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "react-toastify/dist/ReactToastify.css";
+import { GlobalProvider } from "@/context/GlobalContext";
 
 export const metadata = {
     title: 'Property Pulse',
@@ -14,14 +15,16 @@ export const metadata = {
 const MainLayout = ({children}) => {
     return (
         <AuthProvider>
-            <html>
-                <body>
-                    <Navbar />
-                    <main>{children}</main>
-                    <Footer />
-                    <ToastContainer />
-                </body>
-            </html>
+            <GlobalProvider>
+                <html>
+                    <body>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Footer />
+                        <ToastContainer />
+                    </body>
+                </html>
+            </GlobalProvider>
         </AuthProvider>
     )
 }
